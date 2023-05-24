3a361efaec6e2a9a3de679c7f71653f99fae0764
Nityaa1802 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-05-24 19:24:00 +0530

    Started with home page and global css file

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home
+ = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share 
+        <br className='max-md:hidden' />
+        <span className='orange_gradient text_center'>AI-Powered Prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Promptopia is an open-source AI promptin tool for mordern world to discover, create and share crative prompts
+      </p>
+  </section>
+  )
+}
+
+export default Home
